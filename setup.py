61741c1668baf7be03d372a2f98530b6d8067958
Christopher Hoskin <mans0954@debian.org>
2017-11-11 14:06:59 +0000

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 
 setup(
-	name='Phased-Graphviz',
-	version='0.0.0',
-	packages=['phased_graphviz'],
+  name='Phased-Graphviz',
+  version='0.0.0',
+  packages=['phased_graphviz'],
+  install_requires=[
+    'pydot'
+  ]
 )
